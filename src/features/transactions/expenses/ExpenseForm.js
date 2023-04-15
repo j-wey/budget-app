9c369f47ff42a6809f5a5ba2bfa2b6860a700c9e
Jesse Wey <jtwey08@gmail.com>
2023-04-14 21:45:32 -0700

    added ext db + fetch logic for exp/inc; styling

--- src/features/transactions/expenses/ExpenseForm.js
+++ src/features/transactions/expenses/ExpenseForm.js
@@ -1,6 +1,6 @@
 import { Formik, Form, Field } from 'formik'
 import { useDispatch } from 'react-redux'
-import { Row, Col, Button } from 'reactstrap'
+import { Row, Col, Button, CardBody } from 'reactstrap'
 import { addExpense } from './expensesSlice'
 
 const ExpenseForm = () => {
@@ -18,70 +18,72 @@ const ExpenseForm = () => {
     }
 
     return (
-        <Formik
-            initialValues={{
-                account: 'Account...',
-                amount: '0.00',
-                categroy: '',
-                date: 'mm/dd',
-                transType: 'expense',
-                vendor: ''
-            }}
-            onSubmit={handleSubmit}
-        >
-            <Form>
-                <Row>
-                    <Col xs='8'>
-                        <Field
-                            name='vendor'
-                            type='text'
-                            className='form-control'
-                            placeholder='Vendor'
-                        />
-                        <Field
-                            name='category'
-                            as='select'
-                            className='form-control'
-                            defaultValue='Category...'
-                        >
-                            <option disabled>Category...</option>
-                            <option>Housing</option>
-                            <option>Utilities</option>
-                            <option>Food</option>
-                            <option>Recreation</option>
-                            <option>Discretionary</option>
-                            <option>Savings & Investment</option>
-                        </Field>
-                        <Field
-                            name='account'
-                            as='select'
-                            className='form-control'
-                        >
-                            <option disabled>Account...</option>
-                            <option>Alaska Card</option>
-                            <option>Venture Card</option>
-                            <option>AmEx Card</option>
-                            <option>Chase Card</option>
-                            <option>Checking</option>
-                            <option>Savings</option>
-                        </Field>
-                    </Col>
-                    <Col xs='4'>
-                        <Field 
-                            name='date'
-                            type='date'
-                            className='form-control'
-                        />
-                        <Field 
-                            name='amount'
-                            type='number'
-                            className='form-control'
-                        />
-                        <Button type='submit'>Submit</Button>
-                    </Col>
-                </Row>
-            </Form>
-        </Formik>
+        <CardBody>
+            <Formik
+                initialValues={{
+                    account: 'Account...',
+                    amount: '0.00',
+                    categroy: '',
+                    date: 'mm/dd',
+                    transType: 'expense',
+                    vendor: ''
+                }}
+                onSubmit={handleSubmit}
+            >
+                <Form>
+                    <Row>
+                        <Col xs='8'>
+                            <Field
+                                name='vendor'
+                                type='text'
+                                className='form-control'
+                                placeholder='Vendor'
+                            />
+                            <Field
+                                name='category'
+                                as='select'
+                                className='form-control my-2'
+                                defaultValue='Category...'
+                            >
+                                <option disabled>Category...</option>
+                                <option>Housing</option>
+                                <option>Utilities</option>
+                                <option>Food</option>
+                                <option>Recreation</option>
+                                <option>Discretionary</option>
+                                <option>Savings & Investment</option>
+                            </Field>
+                            <Field
+                                name='account'
+                                as='select'
+                                className='form-control'
+                            >
+                                <option disabled>Account...</option>
+                                <option>Alaska Card</option>
+                                <option>Venture Card</option>
+                                <option>AmEx Card</option>
+                                <option>Chase Card</option>
+                                <option>Checking</option>
+                                <option>Savings</option>
+                            </Field>
+                        </Col>
+                        <Col xs='4'>
+                            <Field
+                                name='date'
+                                type='date'
+                                className='form-control'
+                            />
+                            <Field
+                                name='amount'
+                                type='number'
+                                className='form-control my-2'
+                            />
+                            <Button type='submit' className='trans-sub-btn'>Submit</Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Formik>
+        </CardBody>
     )
 }
 
--- src/features/transactions/incomes/IncomeForm.js
+++ src/features/transactions/incomes/IncomeForm.js
@@ -16,68 +16,70 @@ const IncomeForm = () => {
         }
         dispatch(addIncome(income))
     }
-    
+
     return (
-        <Formik
-            initialValues={{
-                account: 'Account...',
-                amount: '0.00',
-                categroy: '',
-                date: 'mm/dd',
-                source: '',
-                transType: 'income',
-                vendor: ''
-            }}
-            onSubmit={handleSubmit}
-        >
-            <Form>
-                <Row>
-                    <Col xs='8'>
-                        <Field
-                            name='source'
-                            type='text'
-                            className='form-control'
-                            placeholder='Source'
-                        />
-                        <Field
-                            name='category'
-                            as='select'
-                            className='form-control'
-                            defaultValue='Category...'
-                        >
-                            <option disabled>Category...</option>
-                            <option>Payroll</option>
-                            <option>Bonus</option>
-                            <option>Gift</option>
-                            <option>Reimbursement</option>
-                            <option>Tax Return</option>
-                        </Field>
-                        <Field
-                            name='account'
-                            as='select'
-                            className='form-control'
-                        >
-                            <option disabled>Account...</option>
-                            <option>Checking</option>
-                            <option>Savings</option>
-                        </Field>
-                    </Col>
-                    <Col xs='4'>
-                        <Field 
-                            name='date'
-                            type='date'
-                            className='form-control'
-                        />
-                        <Field 
-                            name='amount'
-                            type='number'
-                            className='form-control'
-                        />
-                        <Button>Submit</Button>
-                    </Col>
-                </Row>
-            </Form>
-        </Formik>
+        <CardBody>
+            <Formik
+                initialValues={{
+                    account: 'Account...',
+                    amount: '0.00',
+                    categroy: '',
+                    date: 'mm/dd',
+                    source: '',
+                    transType: 'income',
+                    vendor: ''
+                }}
+                onSubmit={handleSubmit}
+            >
+                <Form>
+                    <Row>
+                        <Col xs='8'>
+                            <Field
+                                name='source'
+                                type='text'
+                                className='form-control'
+                                placeholder='Source'
+                            />
+                            <Field
+                                name='category'
+                                as='select'
+                                className='form-control my-2'
+                                defaultValue='Category...'
+                            >
+                                <option disabled>Category...</option>
+                                <option>Payroll</option>
+                                <option>Bonus</option>
+                                <option>Gift</option>
+                                <option>Reimbursement</option>
+                                <option>Tax Return</option>
+                            </Field>
+                            <Field
+                                name='account'
+                                as='select'
+                                className='form-control'
+                            >
+                                <option disabled>Account...</option>
+                                <option>Checking</option>
+                                <option>Savings</option>
+                            </Field>
+                        </Col>
+                        <Col xs='4'>
+                            <Field
+                                name='date'
+                                type='date'
+                                className='form-control'
+                            />
+                            <Field
+                                name='amount'
+                                type='number'
+                                className='form-control my-2'
+                            />
+                            <Button className='trans-sub-btn'>Submit</Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Formik>
+        </CardBody>
     )
 }
 
